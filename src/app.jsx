417c233934692adeba4d681123bbe33be2b5104e
nguyentrantindev <nguyentrantindev@gmail.com>
2024-06-11 22:36:20 +0700

    fix code

--- src/app.jsx
+++ src/app.jsx
@@ -1,13 +1,24 @@
-import { Page } from "../lib/page";
+import { EditForm } from "../lib";
+import { Page } from "../lib";
 import React from "react";
 
 export class App {
   static Main() {
     var app = new Page();
+    app.EditForm = new EditForm("home");
+    app.EditForm.Policies = [
+      {
+        CanRead: true,
+      },
+    ];
     app.Meta.Layout = () => (
       <>
-        <div className="tab-item" data-name="Test"></div>
-        <div className="tab-item" data-name="Test1"></div>
+        <div className="wrapper">
+          <div className="box-shadow" data-name="Test"></div>
+          <div className="box-shadow datetime-picker" data-name="Test1"></div>
+          <div className="box-shadow" data-name="Test3"></div>
+          <div className="box-shadow" data-name="Test4"></div>
+        </div>
       </>
     );
     app.Meta.Components = [
@@ -20,6 +31,37 @@ export class App {
         ComponentType: "Datepicker",
         FieldName: "Test1",
         Id: "1",
+      },
+      {
+        ComponentType: "Number",
+        FieldName: "Test3",
+        Id: "1",
+      },
+      {
+        ComponentType: "Dropdown",
+        FieldName: "Test4",
+        FormatData: "{Label}",
+        Id: "1",
+        LocalQuery: [
+          {
+            Id: "1",
+            Label: "Test",
+          },
+        ],
+        Columns: [
+          {
+            FieldName: "Id",
+            Id: 0,
+            Label: "Id",
+            ComponentType: "Input",
+          },
+          {
+            FieldName: "Label",
+            Label: "Label",
+            Id: 1,
+            ComponentType: "Input",
+          },
+        ],
       },
     ];
     app.Render();
