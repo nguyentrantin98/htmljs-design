b963cbcbdf70653f6d6413647df44f2cef338848
nguyentrantindev <nguyentrantindev@gmail.com>
2024-06-14 16:08:23 +0700

    remove compression2

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,6 @@
 import { defineConfig } from 'vite'
-import { compression } from 'vite-plugin-compression2'
 
 export default defineConfig({
   plugins: [
-    // ...your plugin
-    compression()
   ]
 });
