fe8e9f0bd7745c87eb4e120b5b3af9d71cf64a1e
nguyentrantin98 <nguyentrantindev@gmail.com>
2025-03-08 09:06:24 +0700

    feat: refactor currency handling in SearchEntry and ListViewItem, and remove unused Netlify configuration files

--- test.js
+++
@@ -1,6 +0,0 @@
-debugger;
-const vndAmount = this.Meta.ChildHeader.reduce((sum, header) => {
-    const fieldValue = this.Entity[header.FieldName] || 0;
-    return sum + Number(fieldValue);
-}, 0).toFixed(0).replace(/\B(?=(\d{3})+(?!\d))/g, ',');
-return vndAmount == '0' ? '' : vndAmount;
